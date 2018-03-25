fa1be75e84f9db13adfcdbc122fb403471ccd1d3
Michael Rodler <michael.rodler@paluno.uni-due.de>
2018-03-25 21:13:09 +0200

    re-enabled gas, removed some unused code

--- ethersplay/annotator.py
+++ ethersplay/annotator.py
@@ -1,38 +1,6 @@
 from binaryninja import log_error
 
 from constants import ADDR_SZ
-from evm_llil import TrapInstructions
-
-_numberToName = {v: k for k, v in TrapInstructions.iteritems()}
-
-
-def handle_evm_call(instruction, view, function):
-    possible_value = instruction.get_reg_value("evm_call_nr")
-    if (hasattr(possible_value, 'value')):
-        calltype = _numberToName[possible_value.value]
-    else:
-        log_error("couldn't determine syscall number for instruction @ " +
-                  str(instruction.address))
-        return
-
-    comment = ""
-    if calltype in ('CALL', 'CALLCODE', 'DELEGATECALL', 'STATICCALL'):
-        possible_value = instruction.get_reg_value("evm_call_arg1")
-        if (hasattr(possible_value, 'value')):
-            comment += "target = " + hex(possible_value.value)
-        else:
-            comment += "target = <unknown>"
-
-    if calltype in ('CALL', 'CALLCODE'):
-        comment += "\n"
-        possible_value = instruction.get_reg_value("evm_call_arg2")
-        if (hasattr(possible_value, 'value')):
-            comment += "value = " + str(possible_value.value)
-        else:
-            comment += "value = <unknown>"
-
-    if comment:
-        function.set_comment(instruction.address, comment)
 
 
 def get_annotation_for_stack_offset(function, address, offset=0):
@@ -85,12 +53,6 @@ def annotate(view, function):
     if view.arch.name != 'evm':
         log_error("This plugin works only for EVM bytecode")
         return -1
-
-    # registers = bv.platform.system_call_convention.int_arg_regs
-    # for block in function.low_level_il:
-    #     for instruction in block:
-    #         if instruction.operation == LowLevelILOperation.LLIL_SYSCALL:
-    #             handle_evm_call(instruction, view, function)
 
     for inst, address in function.instructions:
         inststr = str(inst[0]).strip()
