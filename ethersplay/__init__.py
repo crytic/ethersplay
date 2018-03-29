58e469f2a0a170c58f28994915e3cbcfc86c3793
Josselin <josselin@trailofbits.com>
2018-03-29 10:32:42 +0100

    Rewrite stack_value_analysis (WIP) - Use of VSA (each stack element is a set, fathers are merged, convergence is bounded) - Add Plugin mode where the user can set the parameters of the analysis (not working due to UI thread error)

--- ethersplay/__init__.py
+++ ethersplay/__init__.py
@@ -1,8 +1,8 @@
 from binaryninja import PluginCommand, Architecture
 
 from printSourceCode import function_source_code_start
-
 from coverage import function_coverage_start
+from stack_value_analysis import function_dynamic_jump_parameterisable_start
 from evm import EVM, EVMView
 
 
@@ -20,6 +20,9 @@ PluginCommand.register("EVM Manticore Highlight",
                        function_coverage_start,
                        is_valid=is_valid_evm)
 
+PluginCommand.register_for_function("EVM Dynamic Jumps Recovery",
+                                    "EVM Dynamic Jumps Recovery",
+                                    function_dynamic_jump_parameterisable_start)
 
 EVM.register()
 EVMView.register()
