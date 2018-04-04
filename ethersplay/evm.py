1f1c992531ea7c0937bb74390318940d3736f92c
Josh Watson <josh@joshwatson.com>
2018-04-04 15:16:03 -0700

    Improve analysis, cleanup file structure
    Added an analysis callback that looks for invalid jumps and patches the binary to jump to an INVALID instruction instead. This analysis then calls analyze_jumps.
    
    Additionally, the analysis callbacks were moved to a separate file to make things more readable.

--- ethersplay/evm.py
+++ ethersplay/evm.py
@@ -3,15 +3,28 @@
 
 from binaryninja import (LLIL_TEMP, Architecture, BinaryView, BranchType,
                          Endianness, InstructionInfo, InstructionTextToken,
-                         InstructionTextTokenType, IntegerDisplayType,
-                         LowLevelILLabel, MediumLevelILOperation, RegisterInfo,
-                         SegmentFlag, SSAVariable, Symbol, SymbolType)
-from evmasm import EVMAsm
-from evmvisitor import EVMVisitor
-from known_hashes import knownHashes
+                         InstructionTextTokenType, LowLevelILLabel,
+                         LowLevelILOperation, RegisterInfo, SegmentFlag)
+
+from .common import EVM_HEADER
+from .evmasm import EVMAsm
+from .analysis import analyze_invalid_jumps
 
 
 def jumpi(il, addr, imm):
+    dest = il.pop(8)
+
+    if len(il) > 0:
+        push = il[len(il)-1]
+    else:
+        push = None
+
+    if (push is not None and
+            push.operation == LowLevelILOperation.LLIL_PUSH and
+            push.src.operation == LowLevelILOperation.LLIL_CONST):
+        dest = il.const(8, push.src.constant)
+        il.append(il.set_reg(8, LLIL_TEMP(0), il.pop(8)))
+
     t = LowLevelILLabel()
     f = il.get_label_for_address(Architecture['EVM'], addr+1)
     must_mark = False
@@ -21,9 +34,8 @@ def jumpi(il, addr, imm):
         must_mark = True
 
     # We need to use a temporary register here. The il.if_expr() helper
-    # function makes a tree and evaluates the condition's il.pop(32)
+    # function makes a tree and evaluates the condition's il.pop()
     # first, but dest needs to be first.
-    dest = il.pop(8)
     il.append(il.set_reg(8, LLIL_TEMP(addr), dest))
 
     il.append(il.set_reg(8, LLIL_TEMP(0), il.pop(8)))
@@ -84,6 +96,21 @@ def swap(il, addr, distance):
 
 def jump(il, addr, imm):
     dest = il.pop(8)
+
+    if len(il) > 0:
+        push = il[len(il)-1]
+    else:
+        push = None
+
+    if (push is not None and
+            push.operation == LowLevelILOperation.LLIL_PUSH and
+            push.src.operation == LowLevelILOperation.LLIL_CONST):
+        dest = il.const(8, push.src.constant)
+        il.append(il.set_reg(8, LLIL_TEMP(0), il.pop(8)))
+
+    # We need to use a temporary register here. The il.if_expr() helper
+    # function makes a tree and evaluates the condition's il.pop()
+    # first, but dest needs to be first.
     il.append(il.set_reg(8, LLIL_TEMP(addr), dest))
 
     il.append(il.jump(il.reg(8, LLIL_TEMP(addr))))
@@ -113,6 +140,9 @@ insn_il = {
         ),
         'LT': lambda il, addr, imm: il.push(
             8, il.compare_unsigned_less_than(8, il.pop(8), il.pop(8))
+        ),
+        'ISZERO': lambda il, addr, imm: il.push(
+            8, il.compare_equal(8, il.pop(8), il.const(8, 0))
         ),
         'POP': lambda il, addr, imm: il.pop(8),
         'MSTORE': mstore,
@@ -301,18 +331,19 @@ class EVMView(BinaryView):
 
         file_size = len(self.raw)
         self.add_auto_segment(
-            0, file_size-3, 3, file_size,
+            0, file_size - len(EVM_HEADER),
+            len(EVM_HEADER), file_size,
             (SegmentFlag.SegmentReadable |
                 SegmentFlag.SegmentExecutable)
         )
 
-        self.add_analysis_completion_event(analyze)
+        self.add_analysis_completion_event(analyze_invalid_jumps)
         return True
 
     @staticmethod
     def is_valid_for_data(data):
         file_header = data.read(0, 3)
-        if file_header == 'EVM':
+        if file_header == EVM_HEADER:
             return True
         return False
 
@@ -321,146 +352,3 @@ class EVMView(BinaryView):
 
     def get_entry_point(self):
         return 0
-
-
-def build_bb_lookup_table(mlil_function):
-    lookup_table = [None] * len(mlil_function)
-    for bb in mlil_function:
-        for i in xrange(bb.start, bb.end):
-            lookup_table[i] = bb
-    return lookup_table
-
-
-def get_stack_def_for_offset(il, stack_offset):
-    dispatcher = il.function.non_ssa_form
-
-    stack_var = il.get_var_for_stack_location(
-        stack_offset
-    )
-
-    stack_var_version = il.get_ssa_var_version(
-        stack_var
-    )
-
-    ssa_stack_var = SSAVariable(stack_var, stack_var_version)
-
-    hash_def = dispatcher.get_ssa_var_definition(
-        ssa_stack_var
-    )
-
-    hash_il = dispatcher[hash_def] if hash_def is not None else None
-
-    if (hash_il is None or hash_il.src.operation not in
-            (MediumLevelILOperation.MLIL_CONST,
-             MediumLevelILOperation.MLIL_CONST_PTR)):
-        return None
-
-    return hash_il.src
-
-
-def analyze(completion_event):
-    view = completion_event.view
-
-    view.define_auto_symbol(
-        Symbol(
-            SymbolType.FunctionSymbol,
-            0,
-            '_dispatcher'
-        )
-    )
-
-    dispatch_functions = []
-
-    dispatcher = view.get_function_at(0).medium_level_il
-
-    il_bb_lookup = build_bb_lookup_table(dispatcher)
-
-    # Iterate over all of the MLIL instructions and find all the JUMPI
-    # instructions.
-    current_bb = dispatcher.basic_blocks[0]
-    while current_bb:
-        il = current_bb[-1]
-        if il.operation == MediumLevelILOperation.MLIL_IF:
-            visitor = EVMVisitor(lookup=il_bb_lookup)
-            visit_result = visitor.visit(il)
-
-            if visit_result is None:
-                current_bb = il_bb_lookup[il.false]
-                continue
-
-            value, hash_constant = visit_result
-
-            # Locate the definition of the hash value so we can set
-            # the int display type of the hash to a pointer. This
-            # will let us display the hash name there as a different
-            # color.
-            stack_offset = dispatcher.source_function.get_reg_value_at(
-                hash_constant.address, 'sp'
-            ).offset
-
-            hash_il = get_stack_def_for_offset(hash_constant, stack_offset)
-
-            if hash_il is None:
-                stack_offset += 8
-                hash_il = get_stack_def_for_offset(hash_constant, stack_offset)
-
-            hash_value = '#{:08x}'.format(value)
-
-            # XXX: knownHashes uses a string of the hex as a key, it
-            # is probably faster to use an int
-            hash_hex = hex(value).replace('L', '')
-
-            # Find the method name if it's known. Otherwise, just use
-            # the hash.
-            if hash_hex in knownHashes:
-                method_name = knownHashes[hash_hex]
-            else:
-                method_name = hash_value
-
-            # We use SymbolType.ImportedFunctionSymbol because it will
-            # change the font color to orange. Gives it a little "pop"!
-            #
-            # ...yeah that's some stack humor for you.
-            view.define_user_symbol(
-                Symbol(
-                    SymbolType.ImportedFunctionSymbol,
-                    value,
-                    '{} -> {}'.format(
-                        hash_value, method_name
-                    )
-                )
-            )
-
-            if hash_il is not None:
-                # Change the hash operand to display the Symbol.
-                dispatcher.source_function.set_int_display_type(
-                    hash_il.address,
-                    hash_constant.constant,
-                    0,
-                    IntegerDisplayType.PointerDisplayType
-                )
-
-            # The dispatched function is down the True branch.
-            target = dispatcher[il.true]
-
-            # Make a function at the instruction following the
-            # JUMPTARGET instruction. This makes the control flow graph
-            # "fall through" to the function, pruning those basic blocks
-            # from the dispatcher function.
-            if target.operation == MediumLevelILOperation.MLIL_JUMP_TO:
-                dispatch_functions.append(
-                    (target.dest.constant + 1, method_name)
-                )
-
-            current_bb = il_bb_lookup[il.false]
-
-        else:
-            current_bb = None
-
-    # Go over all the functions we noted above and actually create the function
-    # there. We do this last to make sure that view.mlil_instructions doesn't
-    # get messed with while we're iterating over it.
-    for addr, method_name in dispatch_functions:
-        view.create_user_function(addr)
-        dispatch_function = view.get_function_at(addr)
-        dispatch_function.name = method_name
