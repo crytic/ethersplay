34c7775b1dc7b942fcbd88c5f56f3b75900af10a
Josh Watson <josh@joshwatson.com>
2018-05-04 13:06:25 -0700

    Simplified Print Stack plugin command

--- ethersplay/print_stack.py
+++ ethersplay/print_stack.py
@@ -1,24 +1,15 @@
-from binaryninja.interaction import get_open_filename_input, get_text_line_input
-from binaryninja import HighlightStandardColor
-from binaryninja import log
-
-class PrintStack(object):
-    '''
-        Print up to 10 values of the stack
-    '''
-
-    def __init__(self, view, func):
-        self.view = view
-        self.func = func
-
-    def printStack(self):
-        stackOut = self.view.query_metadata(self.func.name+".out")
-
-        for (k, vals) in stackOut.iteritems():
-            vals = [[long(y) for y in x[1::]] if x[0] else None for x in vals]
-            self.func.set_comment(long(k), "Stack %s"%str(vals[-10::]))
+from binaryninja import log_error
 
 
 def function_printStack_start(view, func):
-    detector = PrintStack(view, func)
-    detector.printStack()
+    try:
+        stackOut = view.query_metadata(hex(func.start)+".out")
+    except KeyError:
+        log_error(
+            "Stack information is not available for {}".format(hex(func.start))
+        )
+        return
+
+    for (k, vals) in stackOut.iteritems():
+        vals = [[long(y) for y in x[1::]] if x[0] else "Unknown" for x in vals]
+        func.set_comment(long(k), "Stack {!r}".format(vals[-10::]))
