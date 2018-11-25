3d6417aaf0c9caee9d4b94f358560eca9f5a2e3c
Josh Watson <josh@trailofbits.com>
2018-11-25 14:39:17 -0800

    Added flowgraph rendering to omit stack variable/string annotations for cleaner output

--- ethersplay/__init__.py
+++ ethersplay/__init__.py
@@ -4,27 +4,40 @@ from .printSourceCode import function_source_code_start
 from .coverage import function_coverage_start
 from .print_stack import function_printStack_start
 from .evm import EVM, EVMView
+from .flowgraph import render_flowgraphs
 
 
 def is_valid_evm(view, function=None):
     return view.arch == Architecture['EVM']
 
 
-PluginCommand.register("EVM Source Code",
-                       "EVM Source Code Printer.",
-                       function_source_code_start,
-                       is_valid=is_valid_evm)
+PluginCommand.register(
+    r"Ethersplay\Source Code",
+    "EVM Source Code Printer.",
+    function_source_code_start,
+    is_valid=is_valid_evm
+)
 
-PluginCommand.register("EVM Manticore Highlight",
-                       "EVM Manticore Highlight",
-                       function_coverage_start,
-                       is_valid=is_valid_evm)
+PluginCommand.register(
+    r"Ethersplay\Manticore Highlight",
+    "EVM Manticore Highlight",
+    function_coverage_start,
+    is_valid=is_valid_evm
+)
 
-PluginCommand.register_for_function("EVM Print stack",
-                                    "Print up to 10 values of the stack",
-                                    function_printStack_start,
-                                    is_valid=is_valid_evm)
+PluginCommand.register_for_function(
+    r"Ethersplay\Print stack",
+    "Print up to 10 values of the stack",
+    function_printStack_start,
+    is_valid=is_valid_evm
+)
 
+PluginCommand.register(
+    r'Ethersplay\Render Flowgraphs',
+    'Render flowgraphs of every function, removing stack variable annotations',
+    render_flowgraphs,
+    is_valid=is_valid_evm
+)
 
 EVM.register()
 EVMView.register()
